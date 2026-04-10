e3bc463002fc87987b08332ba76eb254a86c3639
mikasa <v.clark08@outlook.com>
2026-04-10 16:36:54 -0400

    reacts/8balls on ping

--- src/events/messageCreate/01-checkPing.ts
+++ src/events/messageCreate/01-checkPing.ts
@@ -10,6 +10,41 @@ export default async function execute(
     interaction: Message
 ) {
     if (interaction.mentions.has(client.user.id) && !interaction.author.bot) {
+        if (interaction.content.includes('?')) {
+            const question = interaction.content;
+            const ballers = [
+                "Without a doubt",
+                "fuck yea [#100percent](https://hashtag)",
+                "You may rely on it",
+                "As I see it, yes",
+                "Most likely",
+                "Outlook good",
+                "Signs point to yes",
+
+                "<@801288893244506162> thoughts?",
+                "Reply hazy, try again",
+                "Ask again later",
+                "Better not tell you now",
+                "Cannot predict now",
+                "Concentrate and ask again",
+                "Seek the answer elsewhere",
+
+                "Don't count on it",
+                "My reply is no",
+                "My sources say no",
+                "Outlook not so good",
+                "Very doubtful",
+                "LMAO not a chance",
+                "Don't keep your hopes up"
+            ]
+            //grab reaction
+            const msgindex = Math.floor(Math.random() * ballers.length);
+            const msgtosend = ballers[msgindex] as string;
+
+            await increment(interaction.author.id, "8balls", 1, 1);
+            await interaction.reply((question) + "\n**🎱 " + msgtosend + "**");
+            return;
+        }
         //get json & member
         const reactions = JSON.parse(fs.readFileSync(reactPath, "utf-8"));
         const member = interaction.member as GuildMember;
