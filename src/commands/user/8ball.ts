838354ada3755c94ae9473b7a053042ab515a81b
mikasa <v.clark08@outlook.com>
2026-05-07 16:02:23 -0400

    v6.0.2a small 8ball change

--- src/commands/user/8ball.ts
+++ src/commands/user/8ball.ts
@@ -2,45 +2,49 @@ import { SlashCommandBuilder, Client, GuildMember, ChatInputCommandInteraction }
 import { increment } from "../utility/stats";
 
 export const data = new SlashCommandBuilder()
-  .setName("8ball")
-  .setDescription("ask the spirits a question")
-  .addStringOption(option =>
-      option.setName("question")
-          .setDescription("so, uhh..."))
+    .setName("8ball")
+    .setDescription("ask the spirits a question")
+    .addStringOption(option =>
+        option.setName("question")
+            .setDescription("so, uhh..."))
 export async function execute(
-  client: Client,
-  interaction: ChatInputCommandInteraction
+    client: Client,
+    interaction: ChatInputCommandInteraction
 ) {
-  const question = interaction.options.getString("question") ?? "";
-  const ballers = [
-      "Without a doubt",
-      "fuck yea [#100percent](https://hashtag)",
-      "You may rely on it",
-      "As I see it, yes",
-      "Most likely",
-      "Outlook good",
-      "Signs point to yes",
+    const question = interaction.options.getString("question") ?? "";
+    const ballers = [
+        //yes
+        "Without a doubt",
+        "fuck yea [#100percent](https://hashtag)",
+        "You may rely on it",
+        "As I see it, yes",
+        "Most likely",
+        "Outlook good",
+        "Signs point to yes",
 
-      "<@801288893244506162> thoughts?",
-      "Reply hazy, try again",
-      "Ask again later",
-      "Better not tell you now",
-      "Cannot predict now",
-      "Concentrate and ask again",
-      "Seek the answer elsewhere",
+        //injoke
+        "<@801288893244506162> thoughts?",
+        
+        //noncommittal
+        "Reply hazy, try again",
+        "Better not tell you now",
+        "Cannot predict now",
+        "Concentrate and ask again",
+        "Seek the answer elsewhere",
 
-      "Don't count on it",
-      "My reply is no",
-      "My sources say no",
-      "Outlook not so good",
-      "Very doubtful",
-      "LMAO not a chance",
-      "Don't keep your hopes up"
-  ]
-  //grab reaction
-  const msgindex = Math.floor(Math.random() * ballers.length);
-  const msgtosend = ballers[msgindex] as string;
+        //no
+        "Don't count on it",
+        "My reply is no",
+        "My sources say no",
+        "Outlook not so good",
+        "Very doubtful",
+        "LMAO not a chance",
+        "Don't keep your hopes up"
+    ]
+    //grab reaction
+    const msgindex = Math.floor(Math.random() * ballers.length);
+    const msgtosend = ballers[msgindex] as string;
 
-  await increment(interaction.user.id, "8balls", 1, 1);
-  await interaction.reply((question ? question + "?" : "") + "\n**🎱 " + msgtosend + "**")
+    await increment(interaction.user.id, "8balls", 1, 1);
+    await interaction.reply((question ? question + "?" : "") + "\n**🎱 " + msgtosend + "**")
 }
